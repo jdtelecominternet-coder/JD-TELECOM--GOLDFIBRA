98703436567bb470f2ae24385abfd75da7f71bc7
Milton Alves <mmm.miltonalves@gmail.com>
2026-05-01 15:29:39 -0300

    fix: forca recarregamento automatico a cada 30 segundos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Forcar recarregamento sem cache a cada 30 segundos
+setInterval(() => {
+  window.location.reload(true);
+}, 30000);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
